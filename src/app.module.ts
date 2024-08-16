b268a9c71fa5adc8e96049c09d4f6ce20d6c2735
IvanRodDev <splml1014@gmail.com>
2024-08-16 16:09:51 -0500

    Only add a module without architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { KingModule } from './king/king.module';
 
 @Module({
   controllers: [],
   providers: [],
+  imports: [KingModule],
 })
 export class AppModule {}
