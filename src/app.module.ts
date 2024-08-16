655260b587796affe04900c51bc0d2e6c1455f6d
IvanRodDev <splml1014@gmail.com>
2024-08-16 15:33:46 -0500

    Initialized project from scratch

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'Ivan',
-      password: '123',
-      database: 'typeOrmDb',
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: false,
-      retryDelay: 3000,
-      retryAttempts: 10
-    }),
-  ],
   controllers: [],
   providers: [],
 })
